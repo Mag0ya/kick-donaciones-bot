63a5fbaaf80092061a3e25759804afc5db7f9b1c
Mag0ya <pesca.guille@gmail.com>
2025-10-07 02:32:20 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    print(data)  # Aquí puedes hacer lo que quieras con los datos recibidos
+    return '', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
